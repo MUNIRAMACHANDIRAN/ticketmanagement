dfaa2f4dbdb979ea38c1dc9df97e379097f804ea
muniyapan.r <muniramachandiran@gmail.com>
2026-01-29 11:58:53 +0530

    Cleanup: Remove temporary OpenRewrite configuration

--- target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (email, password, role) VALUES ('admin@demo.com', 'password', 'ROLE_ADMIN');
-INSERT INTO users (email, password, role) VALUES ('eng1@demo.com', 'password', 'ROLE_ENGINEER');
-INSERT INTO users (email, password, role) VALUES ('viewer@demo.com', 'password', 'ROLE_VIEWER');
-
-INSERT INTO tickets (req_date, ticket_id, user_name, issue, engineer, status) VALUES
-('2025-01-01','33337','Ravi Prakash','Laptop Reset - Reg','Self','Completed');
